5ff080214d7bf8783b12e17cb63085694c4ceb0a
Sudo-Self <sudo-self@proton.me>
2023-10-15 08:10:45 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,13 +130,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            sudo-self.com{' '}
+            CSS{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          a domain home to ideas and creativity.
+         text over .gif that allows typed input
           </p>
         </a>
          <a
