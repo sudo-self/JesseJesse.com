f33ffa170b93002bf43fc97edcae0c1f97af54e9
Sudo-Self <sudo-self@proton.me>
2023-10-15 18:12:39 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,6 +172,40 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <script src="https://formspree.io/js/formbutton-v1.min.js" defer></script>
+<script>
+  /* paste this line in verbatim */
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+  /* customize formbutton below*/     
+  formbutton("create", {
+    action: "https://formspree.io/f/maygayly",
+    title: "How can I help?",
+    fields: [
+      { 
+        type: "email", 
+        label: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "red"
+      },
+      button: {
+        backgroundColor: "red"
+      }
+    }
+  });
+</script>
     </main>
   )
 }
