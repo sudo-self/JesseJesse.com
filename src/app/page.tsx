9e10966abd3098911b39502cab6efd9d5706d5f5
Sudo-Self <sudo-self@proton.me>
2023-10-25 22:28:05 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
           </p>
         </a>
         <a
-          href="https://tunnel.JesseJesse.com/ROMs/"
+          href="https://aliens.JesseJesse.com/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -120,11 +120,11 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-           A secure fileserver. There when u need it. 
+          An NFT collection. 
           </p>
         </a>  
         <a
-          href="https://css.sudo-self.com"
+          href="https://sudo-self.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -136,7 +136,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-         text over .gif that allows typed input
+       a self hosted domain built from nothing
           </p>
         </a>
          <a
@@ -152,7 +152,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          site that allows changing attributes in style.css
+          lucky enough to make the list?
           </p>
         </a>
         <a
