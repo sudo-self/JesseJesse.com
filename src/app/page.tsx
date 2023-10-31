fe4f8a983e6eebae486e4daccbc5c53c041b0716
Sudo-Self <sudo-self@proton.me>
2023-10-31 03:03:50 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,19 +58,19 @@ export default function Home() {
         </a>
 
         <a
-          href="https://colors.jessejesse.com"
+          href="https://xo.jessejesse.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            crayons{' '}
+            Tic-Tac-Toe{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          A online box of crayons with over 30K colors.
+          A online multiplayer game built with durable objects.
           </p>
         </a>
 
